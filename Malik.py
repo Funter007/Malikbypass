3c0881670880d37a841c981ce368253bc2b3514a
FUNTER-MALIK <96922841+Funter007@users.noreply.github.com>
2022-03-05 17:21:27 +0500

    Create Malik.py

---
+++ Malik.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python2
+import re,os,requests
+import time
+from os import system
+from platform import platform
+from time import sleep
+import sys
+sys.platform
+'wind32', 'wind64', 'linux', '32bit', '64bit', 'android'
+import os
+xoo = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if xoo == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    hsp = 'cls'
+    dr = '\\'
+else:
+    hsp = 'clear'
+    dr = '/'
+
+os.system(hsp)
+time.sleep(1)
+print("...")
+time.sleep(0.05)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.05)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Making Connection...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+print("Connecting Server...")
+time.sleep(0.1)
+os.system(hsp)
+
+def banner():
+    print("""
+\033[0;37m            db   db .d8888. d8888b. 
+\033[0;37m            88   88 88'  YP 88  `8D 
+\033[0;32m            88ooo88 `8bo.   88oodD' 
+\033[0;32m            88   88   `Y8b. 88   
+\033[0;37m            88   88 db   8D 88      
+\033[0;37m            YP   YP `8888Y' 88                 
+\033[0;37m------------------------------------------------- 
+\033[0;32m    (~)\033[0;37m ENJOY FREE BYPASSED CAMONDS\033[0;32m (~)
+\033[0;37m-------------------------------------------------   
+""")
+
+def main():
+    os.system("clear")
+    banner()
+    print("\033[0;37m[1] Install James File Cloning\033[0;32m\033[0;37m (\033[0;32m70% OK\033[0;37m)");time.sleep(0.05)
+    print("\033[0;37m[2] Install ADF File Cloning\033[0;32m\033[0;37m  (\033[0;32m40% OK\033[0;37m)");time.sleep(0.05)
+    print("\033[0;37m[3] Install Jutt File Cloning\033[0;32m\033[0;37m (\033[0;32m20% OK\033[0;37m)");time.sleep(0.05)
+    print("\033[0;37m[4] Exit Tool");time.sleep(0.05)
+    print("\033[0;37m-------------------------------------------------");time.sleep(0.05)
+    m()
+def m():
+    user_option = raw_input("\n(~) Select option :- ")
+    if user_option =="1": 
+        os.system("python Pro.py")
+    elif user_option =="2":
+        os.system("python Adf.py")
+    elif user_option =="3":
+        os.system("python2 Juttbrand.py")
+    elif user_option =="4":
+        print("")
+        print("Tool Logout Successful").center(50)
+        time.sleep(1)
+        os.system("exit")
+    else:
+        print("")
+        print("Please select an correct option").center(50)
+        print("")
+        time.sleep(1)
+        main()
+
+if __name__ == '__main__':
+    main()
